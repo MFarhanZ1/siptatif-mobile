a995567d12d39edaa74a155534cefb5a5edc0e61
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2024-06-24 17:20:18 +0700

    feat: adding 1/2 fitur in screen pembimbing

--- lib/screens/pembimbing/main_pembimbing_screen.dart
+++ lib/screens/pembimbing/main_pembimbing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:siptatif_mobile/services/dosen_service.dart';
 import 'package:siptatif_mobile/services/pembimbing_service.dart';
 
 class MainPembimbingScreen extends StatefulWidget {
@@ -10,19 +11,60 @@ class MainPembimbingScreen extends StatefulWidget {
 
 class _MainPembimbingScreenState extends State<MainPembimbingScreen> {
   final PembimbingService _pembimbingService = PembimbingService();
+  final DosenService _dataDosenService = DosenService();
+
   List<dynamic> _pembimbingList = [];
+  List<dynamic> _dosenAllList = [];
+  dynamic _selectedDosen;
+  bool _loadingform = false;
+
   bool _isLoading = true;
   bool _showForm = false; // Tambahkan variabel ini
+  bool _buttonUpdate = false; // Tambahkan variabel ini
+
+  String _inputValue = '';
+  // callback dari list
+  void _onUpdateButtonPressed(Map<String, dynamic> item) {
+    setState(() {
+      _showForm = !_showForm;
+      _selectedDosen = item;
+      _buttonUpdate = !_buttonUpdate;
+    });
+  }
+
+  void _onDeleteButtonPressed(Map<String, dynamic> item) {
+    setState(() {
+      _selectedDosen = item;
+    });
+    _pembimbingService.deletePembimbing(_selectedDosen['nidn']);
+  }
 
   @override
   void initState() {
     super.initState();
-    _getAllPenguji(); // Panggil metode async di dalam initState
+    _getAllPembimbing(); // Panggil metode async di dalam initState
+    getAllDataDosen();
   }
 
-  Future<void> _getAllPenguji() async {
+  void getAllDataDosen() async {
     try {
-      await _pembimbingService.getAllPenguji().then((value) {
+      await _dataDosenService.getAllDosen().then((value) {
+        print(value.data['results']);
+        setState(() {
+          _dosenAllList = value.data['results'] ?? [];
+          if (_dosenAllList.isNotEmpty) {
+            _selectedDosen = _dosenAllList[0];
+          }
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _getAllPembimbing() async {
+    try {
+      await _pembimbingService.getAllPembimbing().then((value) {
         setState(() {
           _pembimbingList = value.data['results'] ?? [];
           _isLoading = false; // Set _isLoading ke false setelah data dimuat
@@ -77,15 +119,210 @@ class _MainPembimbingScreenState extends State<MainPembimbingScreen> {
               child: RefreshIndicator(
                 child: _isLoading // Tampilkan loading jika sedang memuat data
                     ? Center(child: CircularProgressIndicator())
-                    : ListPembimbing(_pembimbingList),
-                onRefresh: _getAllPenguji,
+                    : ListPembimbing(_pembimbingList, _onUpdateButtonPressed,
+                        _onDeleteButtonPressed),
+                onRefresh: _getAllPembimbing,
               ),
               // Tampilkan data jika sudah selesai memuat
             ),
-          ] else
-            ...[
-
-            ],
+          ] else ...[
+            SingleChildScrollView(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Pilih Dosen Pembimbing',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                          letterSpacing: -0.5,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      if (!_buttonUpdate) ...[
+                        DropdownButton<dynamic>(
+                          value: _selectedDosen,
+                          items: _dosenAllList.map((e) {
+                            return DropdownMenuItem<dynamic>(
+                              value: e,
+                              child: Text(e['nama']),
+                            );
+                          }).toList(),
+                          onChanged: (dynamic? val) {
+                            setState(() {
+                              _selectedDosen = val;
+                            });
+                          },
+                        ),
+                      ] else ...[
+                        Text(
+                          'Nama',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            letterSpacing: -0.5,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          child: TextField(
+                            controller: TextEditingController(
+                                text: _selectedDosen?['nama']),
+                            enabled: false,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              hintText: 'Nama Dosen',
+                            ),
+                          ),
+                        ),
+                      ]
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'NIDN Pembimbing',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      letterSpacing: -0.5,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  SizedBox(
+                    child: TextField(
+                      controller:
+                          TextEditingController(text: _selectedDosen?['nidn']),
+                      enabled: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        hintText: 'NIDN Pembimbing',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'KUOTA',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      letterSpacing: -0.5,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  SizedBox(
+                    child: TextField(
+                      style: TextStyle(height: 1),
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        setState(() {
+                          _inputValue = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        hintText: 'KUOTA Pembimbing',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _showForm = false;
+                          });
+                        },
+                        child: Text('Kembali'),
+                      ),
+                      const SizedBox(width: 8),
+                      if (_buttonUpdate) ...[
+                        ElevatedButton(
+                          onPressed: () {
+                            _pembimbingService.updatePembimbing(
+                                {"kuota": _inputValue},
+                                _selectedDosen?['nidn']);
+                          },
+                          child: Text('Update'),
+                        ),
+                      ] else ...[
+                        ElevatedButton(
+                          onPressed: () {
+                            _loadingform = true;
+                            _pembimbingService.createPembimbing({
+                              "nidn": _selectedDosen?['nidn'],
+                              "kuota": _inputValue
+                            }).then((value) {
+                              _loadingform = false;
+                              print(value.data['response']);
+                              if (value.data['response']) {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text('Sukses'),
+                                      content: Text('Data berhasil terkirim!'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              _showForm = false;
+                                              Navigator.of(context).pop();
+                                            }); // Kembali ke layar sebelumnya
+                                          },
+                                          child: Text('OK'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              } else {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text('Error'),
+                                      content: Text(
+                                          'Terjadi kesalahan saat mengirim data'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              _showForm = false;
+                                              Navigator.of(context).pop();
+                                            }); // Tutup dialog
+                                          },
+                                          child: Text('OK'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              }
+                            });
+                            // Handle kirim action
+                          },
+                          child: Text('Kirim'),
+                        ),
+                      ]
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ]),
       ),
       floatingActionButton: FloatingActionButton(
@@ -101,14 +338,18 @@ class _MainPembimbingScreenState extends State<MainPembimbingScreen> {
   }
 }
 
-Widget ListPembimbing(List<dynamic> pembimbingList) {
+// list view dari pembimbing
+Widget ListPembimbing(
+    List<dynamic> pembimbingList,
+    Function(Map<String, dynamic>) onEditButtonPressed,
+    Function(Map<String, dynamic>) onDeleteButtonPressed) {
   return ListView.builder(
     shrinkWrap: true,
     itemCount: pembimbingList.length,
     itemBuilder: (BuildContext context, int index) {
       var pembimbing = pembimbingList[index];
-      var ndin = pembimbing['nidn'] ?? 'N/A';
-      var nama = pembimbing['nama'] ?? 'N/A';
+      var ndin = pembimbing['nidn'] ?? '-';
+      var nama = pembimbing['nama'] ?? '-';
       var kuota_awal = pembimbing['kuota_awal'] ?? 0;
       var keahlian = pembimbing['keahlian'] ?? 'Keahlian Belum Diisi';
 
@@ -165,7 +406,6 @@ Widget ListPembimbing(List<dynamic> pembimbingList) {
                               )
                             ],
                           ),
-                          
                         ],
                       )
                     ],
@@ -208,10 +448,7 @@ Widget ListPembimbing(List<dynamic> pembimbingList) {
                       const Spacer(),
                       IconButton.filledTonal(
                         onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            "/penguji-update-screen",
-                          );
+                          onEditButtonPressed(pembimbing);
                         },
                         icon: Icon(Icons.edit_note_outlined),
                       ),
@@ -227,7 +464,7 @@ Widget ListPembimbing(List<dynamic> pembimbingList) {
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: <Widget>[
                                     const Text(
-                                        'Apakah anda yakin ingin menghapus data dosen penguji ini?'),
+                                        'Apakah anda yakin ingin menghapus data dosen pembimbing ini?'),
                                     const SizedBox(height: 15),
                                     TextButton(
                                       onPressed: () {
@@ -249,6 +486,7 @@ Widget ListPembimbing(List<dynamic> pembimbingList) {
                                     ),
                                     TextButton(
                                       onPressed: () {
+                                        onDeleteButtonPressed(pembimbing);
                                         Navigator.pop(context);
                                       },
                                       child: Container(
--- lib/services/pembimbing_service.dart
+++ lib/services/pembimbing_service.dart
@@ -5,16 +5,51 @@ import 'package:siptatif_mobile/configs/secure_storage.dart';
 class PembimbingService {
   final _dio = DioClient.instance;
 
-  Future<Response<dynamic>> getAllPenguji() async {
-      String? accessToken = await SecureStorage().readSecureData('accessToken');
+  Future<Response<dynamic>> getAllPembimbing() async {
+    String? accessToken = await SecureStorage().readSecureData('accessToken');
 
     // Menambahkan header Authorization ke dalam permintaan
     Options options = Options(headers: {
       'Authorization': 'Bearer $accessToken',
     });
-    return await _dio.get(
+    return await _dio.get("/pembimbing", options: options);
+  }
+
+  Future<Response<dynamic>> createPembimbing(Map<String, dynamic> data) async {
+    String? accessToken = await SecureStorage().readSecureData('accessToken');
+
+    // Menambahkan header Authorization ke dalam permintaan
+    Options options = Options(headers: {
+      'Authorization': 'Bearer $accessToken',
+    });
+    return await _dio.post(
       "/pembimbing",
-      options: options
+      data: data,
+      options: options,
+    );
+  }
+  Future<Response<dynamic>> updatePembimbing(Map<String, dynamic> data, nidn) async {
+    String? accessToken = await SecureStorage().readSecureData('accessToken');
+
+    // Menambahkan header Authorization ke dalam permintaan
+    Options options = Options(headers: {
+      'Authorization': 'Bearer $accessToken',
+    });
+    return await _dio.put(
+      "/pembimbing/$nidn",
+      data: data,
+      options: options,
+    );
+  }
+
+  Future<Response<dynamic>> deletePembimbing(nidn) async {
+    String? accessToken = await SecureStorage().readSecureData('accessToken');
+    Options options = Options(headers: {
+      'Authorization': 'Bearer $accessToken',
+    });
+    return await _dio.delete(
+      "/pembimbing/$nidn",
+      options: options,
     );
   }
 }
