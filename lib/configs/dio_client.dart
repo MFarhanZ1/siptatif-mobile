c6c9c184bee5a1c71b393a2e097fb76873cb43de
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-24 09:15:38 +0700

    feat: adding show modal dialog instead of auto redirect to login in order user more convenience

--- lib/configs/dio_client.dart
+++ lib/configs/dio_client.dart
@@ -1,9 +1,8 @@
-import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:siptatif_mobile/components/session_expires_dialog_component.dart';
 import 'package:siptatif_mobile/configs/secure_storage.dart';
-import 'package:siptatif_mobile/screens/auth/login_screen.dart';
 import 'package:siptatif_mobile/services/auth_service.dart';
 
 class DioClient {
@@ -52,29 +51,10 @@ class DioClient {
 
           return handler.resolve(await _dio.fetch(e.requestOptions));
         } else {
-          await SecureStorage().deleteAllSecureData();
           if (context.mounted) {
-            var snackBar = SnackBar(
-              /// need to set following properties for best effect of awesome_snackbar_content
-              elevation: 0,
-              behavior: SnackBarBehavior.floating,
-              backgroundColor: Colors.transparent,
-              content: AwesomeSnackbarContent(
-                title: 'Waduh, sesi habis!',
-                message: "${newAccessToken.data['message']}",
-                /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
-                contentType: ContentType.failure,
-              ),
-            );
-
-            // showing snackbar to bottom of screen
-            ScaffoldMessenger.of(context)
-              ..hideCurrentSnackBar()
-              ..showSnackBar(snackBar);
-
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => const LoginScreen()),
-              (Route<dynamic> route) => false,
+            showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => expireDialog(context),
             );
           }
           return;
